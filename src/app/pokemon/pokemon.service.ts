845d4488e25281eaf8239ef0ae83891e4cf428d5
Najmi19 <najmimehdi70@gmail.com>
2023-11-13 22:29:14 +0100

    set up the service

---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  getPokeomnList(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getPokemonById(pokemonId: number): Pokemon|undefined {
+    return POKEMONS.find(Pokemon => Pokemon.id == pokemonId);
+  }
+
+  getPokemonTypeList(): string[] {
+    return[
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy'
+    ]
+  }
+}
