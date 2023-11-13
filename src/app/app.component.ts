f2446989cc179fe36f6c1900a27e30be8313aa56
Najmi19 <najmimehdi70@gmail.com>
2023-11-13 11:40:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { POKEMONS } from './pokemon/mock-pokemon-list';
+import { Pokemon } from './pokemon/pokemon';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1> Liste de pokémons</h1>`
+  templateUrl: 'app.component.html'
 })
 
-export class AppComponent implements OnInit {
-  pokemonList : Pokemon[] = POKEMONS;
+export class AppComponent  {
+
 
   constructor(){
 
-  }
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0])
-   
-      
-  }
-
-  selectPokemon(pokemon : Pokemon){
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
-    
   }
 }
